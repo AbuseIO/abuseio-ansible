5728a01263986a711d2a64aa4542da649352632e
Bart Vrancken <bart@abuse.io>
2017-10-31 20:35:25 +0100

    add oneshot install cmd

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+echo ***
+echo * Magic is starting
+echo ***
+apt-get update
+apt-get install software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible git
+git clone https://github.com/AbuseIO/abuseio-ansible.git /opt/abuseio-ansible
+echo ***
+echo * edit the file /opt/abuseio-ansible/vars/abuseio.yml and take care of the 
+echo * values. Make sure your hostname is valid and has a A/AAAA record which is
+echo * bound to this machine. Once completed you can complete the install by running:
+echo * 
+echo * root@ubuntu:/opt/abuseio-ansible# ansible-playbook -i hosts playbook.yml
+echo * 
+echo * Ran into problems? pastebin your entire output and submit an issue
+echo ***
