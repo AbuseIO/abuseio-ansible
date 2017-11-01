c0c706a6c337c6bfa3dc85906fc3352372e80068
Bart Vrancken <bart@abuse.io>
2017-11-01 23:40:27 +0100

    escape

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-echo ***
-echo * Magic is starting
-echo ***
+echo "***"
+echo "* Magic is starting"
+echo "***"
 apt-get update
 apt-get install software-properties-common
 apt-add-repository -y ppa:ansible/ansible
@@ -14,15 +14,15 @@ apt-get update
 apt-get install -y ansible git python-netaddr python-dnspython
 git clone https://github.com/AbuseIO/abuseio-ansible.git /opt/abuseio-ansible
 cd /opt/abuseio-ansible
-echo '***'
-echo '* edit the file /opt/abuseio-ansible/vars/abuseio.yml and take care of the' 
-echo '* values. Make sure your hostname is valid and has a A/AAAA record which is'
-echo '* bound to this machine. Once completed you can complete the install by running:'
-echo '* '
-echo '* root@ubuntu:/opt/abuseio-ansible# ansible-playbook -i hosts playbook.yml'
-echo '*'
-echo '* PLEASE NOTE: The entire playbook can run over 30 minutes, so be patient!'
-echo '* '
-echo '* Ran into problems? pastebin your entire output and submit an issue'
-echo '***'
+echo "***"
+echo "* edit the file /opt/abuseio-ansible/vars/abuseio.yml and take care of the'"
+echo "* values. Make sure your hostname is valid and has a A/AAAA record which is"
+echo "* bound to this machine. Once completed you can complete the install by running:"
+echo "* "
+echo "* root@ubuntu:/opt/abuseio-ansible# ansible-playbook -i hosts playbook.yml"
+echo "* "
+echo "* PLEASE NOTE: The entire playbook can run over 30 minutes, so be patient!"
+echo "* "
+echo "* Ran into problems? pastebin your entire output and submit an issue"
+echo "***"
 
